e61af3b7f06add580510018aaf07329dbaa27c08
flow <503291753@qq.com>
2018-08-29 15:18:11 +0800

    aa

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This document tries to cut down calculation time of solving a matrix by caching the calculation result in advance.
 
-## Write a short comment describing this function
+## This function is actually a list of functions trying to: 1.get & set a matrix 2.get & set the inverse of this matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function aims at solving a matrix but at first tests if the matrix has been calculated before.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+## Return a matrix that is the inverse of 'x'
 }
